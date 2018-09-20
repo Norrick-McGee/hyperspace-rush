338b2533b1c08b9d7d63c3b7aa7922608058aadd
Norrick McGee <Norrick.McGee@gmail.com>
2018-09-20 06:39:15 -0800

    first commit

---
+++ window.py
@@ -0,0 +1,41 @@
+import pygame
+pygame.init()
+
+display_height = 600
+display_width = 800
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255, 0 , 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+ship_img =pygame.image.load('purp-triangle.png')
+
+def ship(x,y):
+    game_display.blit(ship_img, (x,y))
+
+ship_x = (display_width * 0.45)
+ship_y = (display_height * 0.8)
+
+game_display = pygame.display.set_mode((display_width,display_width))
+
+pygame.display.set_caption('a bit racey')
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        #print(event)
+
+    
+    game_display.fill(white)
+    ship(ship_x, ship_y)
+    pygame.display.flip()
+    clock.tick(60)
+    
+pygame.quit()
+quit()
