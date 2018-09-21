4a836e23dd4180fe56d7292b3050adf4abda28d6
Norrick McGee <Norrick.McGee@gmail.com>
2018-09-20 19:44:02 -0800

    added everything to a game loop

--- window.py
+++ window.py
@@ -10,51 +10,72 @@ red = (255, 0 , 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
-ship_img =pygame.image.load('purp-triangle.png')
+game_display = pygame.display.set_mode((display_width,display_width))
+pygame.display.set_caption('Hyper-Space RUSH')
+clock = pygame.time.Clock()
+
+#loads my flawless hand-drawn ship image
+ship_img =pygame.image.load('purp-triangle.png')  
+
 
 def ship(x,y):
     game_display.blit(ship_img, (x,y))
 
-ship_x = (display_width * 0.45)
-ship_y = (display_height * 0.8)
 
-game_display = pygame.display.set_mode((display_width,display_width))
+def game_loop():
 
-pygame.display.set_caption('Hyper-Space RUSH')
-clock = pygame.time.Clock()
+    #Sets the default Coord of the ship. 
+    ship_x = (display_width * 0.45)
+    ship_y = (display_height * 0.8)
 
-ship_x_change = 0
+    #initializes the change variables used for movement 
+    ship_x_change = 0
+    ship_y_change = 0
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        if event.type == pygame.KEYDOWN:
-            #turn left
-            if event.key == pygame.K_LEFT:
-                print("ship move left")
-                ship_x_change = -5
+            if event.type == pygame.KEYDOWN:
+                #turn left
+                if event.key == pygame.K_a:
+                    print("ship move left")
+                    ship_x_change -= 5
 
-            elif event.key == pygame.K_RIGHT:
-                print("ship move right")
-                ship_x_change = 5
+                if event.key == pygame.K_d:
+                    print("ship move right")
+                    ship_x_change += 5
+                if event.key == pygame.K_s:
+                    print("ship move down")
+                    ship_y_change += 5
+                if event.key == pygame.K_w:
+                    print("ship move up")
+                    ship_y_change += -5
+                
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                ship_x_change = 0
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    ship_x_change = ship_x_change + 5
+                if event.key == pygame.K_d:
+                    ship_x_change = ship_x_change - 5
+                if event.key == pygame.K_w:
+                    ship_y_change += 5
+                if event.key == pygame.K_s:
+                    ship_y_change -= 5
 
+            
+            #print(event)
 
-        
-        #print(event)
+        ship_x = ship_x_change + ship_x
+        ship_y = ship_y_change + ship_y 
 
-    ship_x = ship_x_change + ship_x
+        game_display.fill(white)
+        ship(ship_x, ship_y)
+        pygame.display.flip()
+        clock.tick(60)
 
-    game_display.fill(white)
-    ship(ship_x, ship_y)
-    pygame.display.flip()
-    clock.tick(60)
-    
+game_loop()  
 pygame.quit()
 quit()
